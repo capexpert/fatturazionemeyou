8fc557de1659cec4f7caf78a967e05e4ecdbcddc
Fattura Facile <fatturafacile95@gmail.com>
2026-03-05 17:16:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6769194344-5f04f",
+  "appId": "1:382813369013:web:7fc6ea02fad92dd1cb84fc",
+  "apiKey": "AIzaSyAdRj3-olZ9MPXI7CvPKevgnCp4XqVHstI",
+  "authDomain": "studio-6769194344-5f04f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "382813369013"
+};
